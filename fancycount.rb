6bf52d85e767daa9bb5b16260c337f4297a4dfa9
unicornzero <rylee.k@gmail.com>
2013-05-28 12:16:19 -0600

    updated fancycount

--- fancycount.rb
+++ fancycount.rb
@@ -1,4 +1,14 @@
-KOUNT_MENU = []
+class MenuCreater
+  attr_accessor :options
+  def initialize
+    @options = {}
+  end
+  def append(newoption)
+    @options.merge(newoption)
+  end
+end
+menu = MenuCreater.new
+
 
 class Kounter
   attr_accessor :count_type, :count_variable, :starting_number, :sequence_length, :sequence
@@ -10,40 +20,57 @@ class Kounter
     @sequence = []
     post_initialize(args)
   end
-
   def post_initialize(*args)
-    # subclass name must append count_menu
   end
-
 end
 
 class Multiplier < Kounter
   attr_accessor :multiplier
   def post_initialize(args={})
     @multiplier = args[:multiplier]
-    KOUNT_MENU << self.class #remove this after create menu creater
+  end
+  def menu_key
+    :multiplier
+  end
+  def menu_value
+    "Multiplier"
   end
 end
-add_menu_multiplier = Multiplier.new #use a menu creater method/class to append the KOUNT_MENU array
+menu.append(multiplier: "Multiplier")
+
+
 class MooseKounter < Kounter
   attr_accessor :moose
   def post_initialize(args={})
     @moose = "Moose"
-    KOUNT_MENU << self.class
+  end
+  def menu_key
+    :moosekounter
+  end
+  def menu_value
+    "Moose Counter"
   end
 end
-add_menu_moosekounter = MooseKounter.new
+menu.append(moosekounter: "Moose Counter")
+
+
 class Addifier < Kounter
 end
 
 class InputEngine
+  attr_accessor :menu
+  def initialize(menu)
+    @menu = menu
+  end
+
   def welcome_sequence
     puts "You are now using Fancy Count, perfect for your fancy counting needs."
   end
 
   def menu_options
     puts "Please choose from one of the following counter options:"
-
+    puts menu
   end
 end
-puts KOUNT_MENU
+prompter = InputEngine.new(menu.options)
+prompter.menu_options

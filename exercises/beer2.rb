1a96fd485abf828e652fd4f68cf7c2b6a61fccb7
unicornzero <rylee.k@gmail.com>
2013-05-29 17:53:02 -0600

    update beer song

--- exercises/beer2.rb
+++ exercises/beer2.rb
@@ -2,29 +2,23 @@ class BottleSong
   attr_accessor :drink, :startnum
 
   def initialize( args={} )
-    @drink = args[:drink] || "Beer"
+    @drink = args[:drink] || "beer"
     @startnum = args[:startnum] || 99
   end
 
   def song
     @startnum.downto(2) do |num|
-      puts "#{num} bottles of #{drink} on the wall."
-      puts "#{num} bottles of #{drink}."
-      puts "Take one down and pass it around."
-      puts "#{num - 1} bottles of #{drink} on the wall."
+      puts "#{num} bottles of #{drink} on the wall, #{num} bottles of #{drink}."
+      puts "Take one down and pass it around. #{num - 1} bottles of #{drink} on the wall."
       puts ""
     end
     num = 2
-      puts "#{num} bottles of #{drink} on the wall."
-      puts "#{num} bottles of #{drink}."
-      puts "Take one down and pass it around."
-      puts "#{num - 1} bottle of #{drink} on the wall."
+      puts "#{num} bottles of #{drink} on the wall. #{num} bottles of #{drink}."
+      puts "Take one down and pass it around. #{num - 1} bottle of #{drink} on the wall."
       puts ""
     num = 1
-      puts "#{num} bottle of #{drink} on the wall."
-      puts "#{num} bottle of #{drink}."
-      puts "Take it down and pass it around."
-      puts "No more bottles of #{drink} on the wall."
+      puts "#{num} bottle of #{drink} on the wall. #{num} bottle of #{drink}."
+      puts "Take it down and pass it around. No more bottles of #{drink} on the wall."
       puts ""
       puts "No more bottles of beer on the wall, no more bottles of beer."
       puts "Go to the store and buy some more, #{@startnum} bottles of beer on the wall."

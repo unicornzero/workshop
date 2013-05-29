cf5b89b75eba2d0de8fccecb814ea77cb6d7937d
unicornzero <rylee.k@gmail.com>
2013-05-28 19:48:35 -0600

    like talking to my brother...

---
+++ exercises/teen_talker.rb
@@ -0,0 +1,41 @@
+puts "Have a conversation with your 16 year old brother, Billy."
+puts "  Hey, what's up?"
+class Billy
+  def self.talk
+    input = gets.chomp
+    responder(input)
+  end
+
+
+  def self.responder(input)
+    case input
+    when /^[A-Z][A-Z]+/
+      yelled_at
+    when /\?/
+      asked_question
+    when /\w/
+      told_something
+    else
+      blah
+    end
+  end
+
+  def self.asked_question
+    puts "  Sure."
+  end
+
+  def self.told_something
+    puts "  Whatever."
+  end
+
+  def self.yelled_at
+    puts "  Woah, chill out!"
+  end
+
+  def self.blah
+    puts "  Fine, be that way."
+  end
+
+end
+
+5.times {Billy.talk}

8d1c1f8618aa8e8de02e5164f30e33b17fc7fff4
unicornzero <rylee.k@gmail.com>
2013-05-29 18:44:34 -0600

    refactored welcome sequence, thanker, and addifier

--- fancycount.rb
+++ fancycount.rb
@@ -1,5 +1,5 @@
 class MenuCreater
-  attr_accessor :options, :user_choice
+  attr_accessor :options, :user_choice, :player
   def initialize
     @options = {}
   end
@@ -9,6 +9,15 @@ class MenuCreater
   end
 
   def welcome_sequence
+    puts "You are now using Fancy Count, perfect for your fancy counting needs."
+    puts "What is your name?"
+    @player = gets.chomp
+    puts ""
+    puts "Hello, #{@player}"
+    selecter_sequence
+  end
+
+  def selecter_sequence
     puts "---------------"
     menu_options
     selection
@@ -27,10 +36,13 @@ class MenuCreater
       case @user_choice 
       when "Moose"
         Moose.countmenu
+        thanker
       when "Multiplier"
         Multiplier.countmenu
+        thanker
       when "Addifier"
         Addifier.countmenu
+        thanker
       else
         puts "Your selection was found in the list, but isn't finished being developed."
       end
@@ -39,14 +51,19 @@ class MenuCreater
     end
   end
 
+  def thanker
+    puts "Thanks for playing, #{@player}!"
+    puts ""
+  end
+
   def playagain?
-    puts "#{PLAYER}, would you like to play again?"
+    puts "#{@player}, would you like to play again?"
     affirmative = ["Yes", "Y", "yes", "y"]
     answer = gets.chomp
     if affirmative.include?(answer)
-      welcome_sequence 
+      selecter_sequence 
     else
-      puts "Goodbye, #{PLAYER}!"
+      puts "Goodbye, #{@player}!"
     end
   end
 end
@@ -75,7 +92,6 @@ class Multiplier < Kounter
     (1..10).each do |n| 
       puts "#{n}.  #{ n * multiplier.to_f}"
     end
-    thanker
   end
 
 end
@@ -92,7 +108,6 @@ class Moose < Kounter
 
   def self.count
     (1..10).each {|n| puts "  " + n.to_s + " Moose!"}
-    thanker
   end
 end
 menu.merger("Moose" => "Moose")
@@ -119,22 +134,10 @@ class Addifier < Kounter
       helpcount = n + helpcount
       puts helpcount
     end
-    thanker
   end
 end
 menu.merger("Addifier" => "Addifier")
 
-def thanker
-  puts "Thanks for playing, #{PLAYER}!"
-  puts ""
-end
-
-##########
-puts "You are now using Fancy Count, perfect for your fancy counting needs."
-puts "What is your name?"
-PLAYER = gets.chomp
-puts ""
-puts "Hello, #{PLAYER}"
 menu.welcome_sequence
 
 
